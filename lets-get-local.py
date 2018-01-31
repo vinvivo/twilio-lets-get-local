9ec0f6477492fcca521b00356335a2f1025c2708
vinney <vinvivo@users.noreply.github.com>
2018-01-30 23:25:41 -0800

    refactor

--- lets-get-local.py
+++ lets-get-local.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 from datetime import datetime
 
-account_sid = "AC05c492100ba229f40bd463f0c99da9b2"
-auth_token = "bf7c0153a0a3ff0dc7fa2ad5e0b532be"
+account_sid = "ACCOUNT_SID"
+auth_token = "AUTH_TOKEN"
 
 client = Client(account_sid, auth_token)
 currenttime = datetime.now().strftime('%H:%M:%S')
 
 message = client.messages.create(
-    to="+14153007522",
+    to="+1234567890",   # Replace phone number
     from_="+14159657284",
     body="Greetings! The current time is: {} NQ3NHO9ABIUJDVV".format(currenttime)
 )
